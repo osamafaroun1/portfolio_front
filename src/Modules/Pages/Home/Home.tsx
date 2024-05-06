9222a2aa86d0873e18412e08041fdb4010902655
nawar <nawaralissa23@gmail.com>
2024-05-06 22:46:10 +0200

    second commit

--- src/Modules/Pages/Home/Home.tsx
+++ src/Modules/Pages/Home/Home.tsx
@@ -1,9 +1,16 @@
 import React, { FunctionComponent } from 'react';
+import img1 from '../src/assets/Hero.png'
+import Hero from '../../Components/Hero/Hero';
 
 const Home: FunctionComponent = () => {
 return (
-    <div></div>
-);
+    <Hero 
+    img={img1}
+    title1=" Software Engineering "
+    title2 =" looking For a FullStack developer"
+    desc= " i have experience in React and Laravel"
+    btn1= " DownLoad Cv "
+/>);
 };
 
 export default Home;
