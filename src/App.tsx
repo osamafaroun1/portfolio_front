e0159f613911c903bc84a80cca330168193140af
hussam-alhor <hosam4255@gmail.com>
2024-05-07 01:45:00 +0200

    about with response hussam

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
 
-import { useState } from 'react'
-import './App.css'
-import AbouteMe from './Modules/Pages/AboutMe/AbouteMe'
-import ReadMore from './Modules/Components/RradMore/ReadMore'
-
 function App() {
-
-const [showFirstComponent, setShowFirstComponent] = useState<boolean>(true)
-
-const switchComponent = () => {
-  setShowFirstComponent(!showFirstComponent)
-}
   return (
     <div>
-    {
-    showFirstComponent?(<AbouteMe onSwitch={switchComponent}/>):(<ReadMore onSwitch={switchComponent}/>)
-    }
     </div>
   )
 
--- src/Modules/Components/RradMore/ReadMore.tsx
+++ src/Modules/Components/RradMore/ReadMore.tsx
@@ -1,89 +1,13 @@
-import { Col, Container, Row } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import './ReadMore.css'
-import { TAboutProps } from '../../Pages/AboutMe/AbouteMe';
-import { FaHtml5 } from "react-icons/fa";
-import { FaCss3Alt } from "react-icons/fa";
-import { FaJsSquare } from "react-icons/fa";
-import { BiLogoTypescript } from "react-icons/bi";
-
-import { SiTypescript } from "react-icons/si";
-import { FaReact } from "react-icons/fa";
-import { FaPhp } from "react-icons/fa";
-import { SiMysql } from "react-icons/si";
-import { FaLaravel } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-
-
+import { data } from './read';
+import { TAboutProps } from '../AboutMe/AbouteMe';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const ReadMore:React.FC<TAboutProps> = ({onSwitch}) => {
-
-    type TItem = {
-        id : number ,
-        title : string,
-        desc : string,
-        icon : any,
-        iconTwo?: any
-    }
-    
-    const data: TItem[] = [
-        {
-            id : 1,
-            title : 'Html5',
-            desc : 'Front end section',
-            icon : FaHtml5
-        },
-        {
-            id : 2,
-            title : 'CSS & Bootstrap',
-            desc : 'Front end section', 
-            icon : FaCss3Alt
-        },
-        {
-            id : 3,
-            title : 'JS & TS',
-            desc : 'Front end section',
-            icon: FaJsSquare ,
-            iconTwo : SiTypescript 
-        },
-    
-        {
-            id : 4,
-            title : 'React',
-            desc : 'Front end section',
-            icon : FaReact
-        },
-        {
-            id : 5,
-            title : 'php',
-            desc : 'Back end section',
-            icon : FaPhp
-        },
-    
-        {
-            id : 6,
-            title : 'MySQL',
-            desc : 'Back end section',
-            icon : SiMysql
-        },
-        {
-            id : 7,
-            title : 'Laravel',
-            desc : 'Back end section',
-            icon : FaLaravel
-        },
-        {
-            id : 8,
-            title : 'Git & GitHup',
-            desc : 'To manage project',
-            icon : FaGithub
-    
-        },
-    ]
-
-  return (
-    <div className='about-color'>
+return (
+    <div className='about-color read-res'>
       <Container>
         
         <Row className='read_More'>
