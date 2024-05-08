10f79096b6e78ac4749d24121b1a94b43cd77cb2
Aya1Khlif <ayakhlife222@gmail.com>
2024-05-08 13:43:41 +0300

    edit app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 
 import './App.css'
-// import { Contact } from './Modules/Components/Contact/Contact'
-import { Login } from './Modules/Pages/Login'
 
 function App() {
   return (
     <>
-     {/* <Contact/> */}
-     <Login/>
+
     </>
   )
 }
