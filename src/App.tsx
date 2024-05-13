5a4aa791e81abe335a21217fb461d9e5b47a4ac6
hussam-alhor <hosam4255@gmail.com>
2024-05-13 16:02:35 +0200

    router hussam

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Route, Routes } from "react-router-dom"
+
 function App() {
   return (
     <div>
-  
+  <Routes>
+    <Route path="/" element=''></Route>
+    <Route path="/login" element=''></Route>
+  </Routes>
     </div>
   )
 
